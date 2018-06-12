009c15cfdb1a4f8b8d16809a54575da6d8570662
sundaka <837136131@qq.com>
2018-06-12 08:00:59 +0800

    add mango factory

---
+++ src/main/java/Mango.java
@@ -0,0 +1,33 @@
+public class Mango {
+    private String taste;
+    private String color;
+    private Double price;
+
+    public Mango(){
+        System.out.println("create a mango");
+    }
+
+    public String getTaste() {
+        return taste;
+    }
+
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
